5b98abb823b710efc04591bd6a33588ce5c5594c
Nguyễn Sơn Lâm <lamns@lumi.biz>
2021-04-20 15:05:51 +0700

    new version

--- app/main.js
+++ app/main.js
@@ -9,7 +9,8 @@ const updater                = require('electron-simple-updater');
 updater.init({
   checkUpdateOnStart: false,
   autoDownload: false,
-  url: 'https://raw.githubusercontent.com/sonlamnguyen/test-update/master/updates.json'
+  url: 'https://raw.githubusercontent.com/sonlamnguyen/test-update/master/updates.json',
+  version: '0.0.1'
 });
 
 let mainWindow;
